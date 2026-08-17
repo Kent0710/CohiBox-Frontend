76b2fde7c0e8a3b8ce292af9633128ae51b03a1f
Kent0710 <christiankent001@gmail.com>
2026-08-17 19:12:59 +0800

    fix: redirect uri inside callback is hardcoded instead of getting the new one from env

--- src/routes/auth/callback/+page.server.ts
+++ src/routes/auth/callback/+page.server.ts
@@ -1,42 +1,46 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { PUBLIC_COGNITO_CLIENT_ID, PUBLIC_COGNITO_DOMAIN } from '$env/static/public';
+import {
+	PUBLIC_COGNITO_CLIENT_ID,
+	PUBLIC_COGNITO_DOMAIN,
+	PUBLIC_COGNITO_REDIRECT_URI
+} from '$env/static/public';
 
 export const load: PageServerLoad = async ({ url, cookies, fetch }) => {
-  const code = url.searchParams.get('code');
-  console.log('SERVER CODE:', code);
+	const code = url.searchParams.get('code');
+	console.log('SERVER CODE:', code);
 
-  if (!code) throw redirect(303, '/login');
+	if (!code) throw redirect(303, '/login');
 
-  const res = await fetch(`${PUBLIC_COGNITO_DOMAIN}/oauth2/token`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams({
-      grant_type: 'authorization_code',
-      client_id: PUBLIC_COGNITO_CLIENT_ID,
-      redirect_uri: 'http://localhost:5173/auth/callback',
-      code
-    })
-  });
+	const res = await fetch(`${PUBLIC_COGNITO_DOMAIN}/oauth2/token`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		body: new URLSearchParams({
+			grant_type: 'authorization_code',
+			client_id: PUBLIC_COGNITO_CLIENT_ID,
+			redirect_uri: PUBLIC_COGNITO_REDIRECT_URI,
+			code
+		})
+	});
 
-  const tokens = await res.json();
-  console.log('SERVER TOKENS:', tokens);
+	const tokens = await res.json();
+	console.log('SERVER TOKENS:', tokens);
 
-  if (!tokens.access_token) throw redirect(303, '/login');
+	if (!tokens.access_token) throw redirect(303, '/login');
 
-  cookies.set('access_token', tokens.access_token, {
-    path: '/',
-    httpOnly: true,
-    sameSite: 'lax',
-    maxAge: 60 * 60
-  });
+	cookies.set('access_token', tokens.access_token, {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'lax',
+		maxAge: 60 * 60
+	});
 
-  cookies.set('id_token', tokens.id_token, {
-    path: '/',
-    httpOnly: true,
-    sameSite: 'lax',
-    maxAge: 60 * 60
-  });
+	cookies.set('id_token', tokens.id_token, {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'lax',
+		maxAge: 60 * 60
+	});
 
-  throw redirect(303, '/dashboard');
+	throw redirect(303, '/dashboard');
 };
